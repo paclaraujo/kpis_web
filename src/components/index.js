29dee1cd254906a00648d20e4a1380e1787f3d7c
Paloma Araujo <paclaraujo@gmail.com>
2022-11-30 14:28:15 -0300

    chore: export base components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button";
+export { default as LineChart } from "./LineChart";
+export { default as Select } from "./Select";
+export { default as TextField } from "./TextField";
+export { default as Card } from "./Card";
